a1533d2f71c59954c4cf57b1ef6b69c4d1fb54aa
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-09-21 11:49:41 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,12 @@
+var ulang = "Semangat..."
+var star = "*****"
+
+console.log(ulang.repeat(5))
+
+for (i=1 ; i<=20 ;i=i+4){
+	console.log(star.repeat(i))
+}
+for (j=13 ; j>=1 ;j=j-4){
+	console.log(star.repeat(j))
+}
+
